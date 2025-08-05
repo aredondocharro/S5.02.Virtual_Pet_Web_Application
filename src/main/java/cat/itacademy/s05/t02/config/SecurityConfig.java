f2e8cbcac9d923b0845f1f13ed22ec639a0c2868
Alejandro Redondo Charro <alejandro.redondo.charro@gmail.com>
2025-08-05 22:49:44 +0200

    added: annotations preauthorize and testing with postman

--- src/main/java/cat/itacademy/s05/t02/config/SecurityConfig.java
+++ src/main/java/cat/itacademy/s05/t02/config/SecurityConfig.java
@@ -3,24 +3,92 @@ package cat.itacademy.s05.t02.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
 public class SecurityConfig {
 
-    @Bean
+/*    @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-        return httpSecurity.build();
+        return httpSecurity
+                .csrf(csrf -> csrf.disable()) // Disable CSRF for simplicity, not recommended for production
+                .httpBasic(Customizer.withDefaults()) // Enable HTTP Basic authentication
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) // Stateless session management
+                .authorizeHttpRequests(auth -> auth
+                        // Configure public endpoints
+                        .requestMatchers("/auth/hello").permitAll() // Allow public access to /auth/hello
+                        // Configure secured endpoints
+                        .requestMatchers("/auth/hello-secured").hasAnyAuthority("READ") // Secure /auth/hello-secured
+                        // Configure the rest of the application
+                        .anyRequest().denyAll() // Deny all other requests
+                )
+                .build();
+    }*/
+
+   @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .csrf(csrf -> csrf.disable()) // Disable CSRF for simplicity, not recommended for production
+                .httpBasic(Customizer.withDefaults()) // Enable HTTP Basic authentication
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) // Stateless session management
+                .build();
     }
 
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
         return authenticationConfiguration.getAuthenticationManager();
+    }
+
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setPasswordEncoder(passwordEncoder());
+        provider.setUserDetailsService(userDetailsService());
+        return provider;
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        List<UserDetails> userDetailsList = new ArrayList<>();
+        userDetailsList.add(User.withUsername("Alex")
+                .password("1234")
+                .roles("ADMIN")
+                .authorities("READ", "CREATE")
+                .build());
+
+        userDetailsList.add(User.withUsername("John")
+                .password("1234")
+                .roles("USER")
+                .authorities("READ")
+                .build());
+
+
+        return new InMemoryUserDetailsManager(userDetailsList);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return NoOpPasswordEncoder.getInstance(); // Note: NoOpPasswordEncoder is not recommended for production use, it's only for demonstration purposes.
+        //return new BCryptPasswordEncoder(); // Use BCryptPasswordEncoder for production
     }
 }
--- src/main/java/cat/itacademy/s05/t02/controller/AuthController.java
+++ src/main/java/cat/itacademy/s05/t02/controller/AuthController.java
@@ -1,18 +1,22 @@
 package cat.itacademy.s05.t02.controller;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
+@PreAuthorize("denyAll()") // Deny all access by default
 public class AuthController {
 
     @GetMapping("/hello")
+    @PreAuthorize("permitAll()") // Allow public access to this endpoint
     public String hello() {
         return "Hello, welcome to the Virtual Pet!";
     }
     @GetMapping("/hello-secured")
+    @PreAuthorize("hasAuthority('READ')") // Allow access only to users with 'READ' authority
     public String helloSecured() {
         return "Hello, welcome to the Virtual Pet Secured!";
     }
