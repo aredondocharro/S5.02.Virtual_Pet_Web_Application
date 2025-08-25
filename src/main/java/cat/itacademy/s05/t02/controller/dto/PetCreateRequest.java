acf8ac336fcf0d9e2def6328778f27524c492887
Alejandro Redondo Charro <alejandro.redondo.charro@gmail.com>
2025-08-25 19:22:13 +0200

    added some features in petcolor

--- src/main/java/cat/itacademy/s05/t02/controller/dto/PetCreateRequest.java
+++ src/main/java/cat/itacademy/s05/t02/controller/dto/PetCreateRequest.java
@@ -3,6 +3,7 @@ package cat.itacademy.s05.t02.controller.dto;
 import cat.itacademy.s05.t02.domain.PetColor;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 @Schema(description = "Payload to create a pet")
 public record PetCreateRequest(
@@ -11,7 +12,12 @@ public record PetCreateRequest(
         @NotBlank
         String name,
 
-        @Schema(description = "Main color", example = "blue", requiredMode = Schema.RequiredMode.REQUIRED)
-        @NotBlank
+        @Schema(
+                description = "Main color",
+                example = "pink",
+                allowableValues = {"pink","black","white","orange"},
+                requiredMode = Schema.RequiredMode.REQUIRED
+        )
+        @NotNull
         PetColor color
 ) {}
--- src/main/java/cat/itacademy/s05/t02/domain/PetColor.java
+++ src/main/java/cat/itacademy/s05/t02/domain/PetColor.java
@@ -1,5 +1,20 @@
 package cat.itacademy.s05.t02.domain;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+
 public enum PetColor {
-    PINK, BLACK, WHITE, ORANGE
+    PINK, BLACK, WHITE, ORANGE;
+
+
+    @JsonCreator
+    public static PetColor from(String value) {
+        return PetColor.valueOf(value.trim().toUpperCase());
+    }
+
+
+    @JsonValue
+    public String toJson() {
+        return name().toLowerCase();
+    }
 }
